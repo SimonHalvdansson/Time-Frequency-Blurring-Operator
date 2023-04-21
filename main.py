9103e0ce44127a9e2732d98cce2368c660fd267e
SimonHalvdansson <49042625+SimonHalvdansson@users.noreply.github.com>
2023-04-21 09:02:17 +0200

    More

--- main.py
+++ main.py
@@ -6,17 +6,15 @@ import matplotlib.pyplot as plt
 import torch
 from torch.utils.data import Dataset, DataLoader
 from pathlib import Path
-from torchvision import datasets, transforms
+from torchvision import datasets, transforms, models
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
-from torchvision import models
 from torchinfo import summary
-import pickle
-import torch
 from functools import partial
 import random
+from scipy.ndimage import gaussian_filter
 
 
 def download_and_save_data():
@@ -31,9 +29,9 @@ def download_and_save_data():
         os.mkdir(folder) 
         
 
-def load_audio_files(path: str, max_images):
+def load_audio_files(path: str, train_im, val_im, test_im):
     #For future, smallest class has 1557 examples
-    dataset = []
+    train, val, test = [], [], []
     
     all_contents = os.listdir(path)
     subdirs = [content for content in all_contents if os.path.isdir(os.path.join(path, content))]
@@ -41,23 +39,30 @@ def load_audio_files(path: str, max_images):
     for subdir in subdirs:
         subpath = os.path.join(path, subdir)
         
-        walker = random.shuffle([str(p) for p in Path(subpath).glob(f'*.wav')])
-        
+        walker = [str(p) for p in Path(subpath).glob(f'*.wav')]
+        random.shuffle(walker)
+                
         for i, file_path in enumerate(walker):
-            if (i >= max_images):
+            waveform, sample_rate = torchaudio.load(file_path)
+            
+            if i < train_im:
+                train.append([waveform, subdir])
+            elif i < train_im + val_im:
+                val.append([waveform, subdir])
+            elif i < train_im + val_im + test_im:
+                test.append([waveform, subdir])
+            else:
                 break
             
-            waveform, sample_rate = torchaudio.load(file_path)
-            dataset.append([waveform, sample_rate, subdir])
             
-    return dataset, subdirs
+    return train, val, test, subdirs
 
-def waveform_to_spectrogram(waveform, sample_rate):
+def waveform_to_spectrogram(waveform):
     # Parameters for the spectrogram
-    n_fft = 4096        # Increased from 2048
+    n_fft = 4096
     win_length = None
-    hop_length = 256    # Decreased from 512
-    n_mels = 256        # Increased from 128
+    hop_length = 256
+    n_mels = 256
     
     
     # Create the MelSpectrogram transform
@@ -81,10 +86,10 @@ def waveform_to_spectrogram(waveform, sample_rate):
     log_mel_spectrogram = torchaudio.transforms.AmplitudeToDB()(mel_spectrogram)
     
     # Shift and normalize the log-mel spectrogram to have values in the range [0, 1]
-    shifted_log_mel_spectrogram = log_mel_spectrogram - log_mel_spectrogram.min()
-    normalized_log_mel_spectrogram = shifted_log_mel_spectrogram / shifted_log_mel_spectrogram.max()
+    log_mel_spectrogram -= log_mel_spectrogram.min()
+    log_mel_spectrogram /= log_mel_spectrogram.max()
 
-    return normalized_log_mel_spectrogram
+    return log_mel_spectrogram
 
 def plot_spectrogram(spec, title=None, ylabel='freq_bin', aspect='auto', xmax=None):
     fig, axs = plt.subplots(1, 1)
@@ -103,7 +108,7 @@ def pad_waveform(waveform, target_length):
         padding_length = target_length - current_length
         waveform = torch.nn.functional.pad(waveform, (0, padding_length))
     return waveform
-    
+
 def one_hot_encode(class_label, labels):
     num_classes = len(labels)
     one_hot_vector = torch.zeros(num_classes)
@@ -115,17 +120,6 @@ def add_noise(waveform):
     return waveform + torch.randn(waveform.size())*0.17*waveform.max()
     
 def add_spec_aug(spectrogram, F=27, T=10, num_freq_masks=2, num_time_masks=2):
-    """
-    Apply SpecAugment to a given spectrogram tensor.
-
-    :param spectrogram: Tensor of shape (1, 256, 63) representing a spectrogram
-    :param F: Maximum width of the frequency mask
-    :param T: Maximum width of the time mask
-    :param num_freq_masks: Number of frequency masks to apply
-    :param num_time_masks: Number of time masks to apply
-    :return: SpecAugmented spectrogram
-    """
-
     augmented_spectrogram = spectrogram.clone()
     _, num_freq_bins, num_time_bins = spectrogram.size()
 
@@ -143,23 +137,64 @@ def add_spec_aug(spectrogram, F=27, T=10, num_freq_masks=2, num_time_masks=2):
 
     return augmented_spectrogram
 
-def augment_waveforms(ds, noise = True):
+def gaussian_kernel(kernel_size, sigma_x, sigma_y):
+    # Create 1D Gaussian kernels for x and y dimensions
+    x = torch.linspace(-kernel_size // 2 + 1, kernel_size // 2, kernel_size)
+    gauss_kernel_x = torch.exp(-x**2 / (2 * sigma_x**2))
+    gauss_kernel_y = torch.exp(-x**2 / (2 * sigma_y**2))
+
+    gauss_kernel_x /= gauss_kernel_x.sum()  # Normalize the kernel for x
+    gauss_kernel_y /= gauss_kernel_y.sum()  # Normalize the kernel for y
+
+    # Create 2D Gaussian kernel
+    gauss_kernel = gauss_kernel_x.view(1, 1, -1) * gauss_kernel_y.view(1, -1, 1)
+
+    return gauss_kernel
+
+def blur_tensor(tensor, kernel_size=5, sigma_x=1.5, sigma_y=1.5):
+    assert len(tensor.shape) == 3, "Input tensor must have 3 dimensions (1, H, W)"
+
+    kernel = gaussian_kernel(kernel_size, sigma_x, sigma_y)
+    kernel = kernel.expand(tensor.shape[0], -1, -1, -1)
+
+    # Pad the tensor to avoid boundary issues during convolution
+    pad_size = kernel_size // 2
+    padded_tensor = F.pad(tensor, (pad_size, pad_size, pad_size, pad_size), mode='reflect')
+
+    # Convolve the tensor with the Gaussian kernel
+    blurred_tensor = F.conv2d(padded_tensor, kernel, stride=1, padding=0, groups=tensor.shape[0])
+
+    return blurred_tensor
+
+def add_spec_blur(spec, sigma_x = 2.0, sigma_y = 1.0):
+    blurred = blur_tensor(spec, sigma_x = sigma_x, sigma_y = sigma_y)
+    
+    #let's normalize again
+    blurred -= blurred.min()
+    blurred /= blurred.max()
+    return blurred
+    
+
+def augment_waveforms(ds, noise = False):
     aug_ds = []
     
-    for X, fs, Y in ds:
+    for X, Y in ds:
         if noise:  
-            aug_ds.append([add_noise(X), fs, Y])
+            aug_ds.append([add_noise(X), Y])
             
-        aug_ds.append([X, fs, Y])
+        aug_ds.append([X, Y])
         
     return aug_ds
 
-def augment_spectrogram(ds, spec_aug = True):
+def augment_spectrogram(ds, spec_aug = False, spec_blur = False):
     aug_ds = []
     
     for X, Y in ds:
         if spec_aug:  
             aug_ds.append([add_spec_aug(X), Y])
+
+        if spec_blur:
+            aug_ds.append([add_spec_blur(X), Y])
             
         aug_ds.append([X, Y])
         
@@ -180,6 +215,172 @@ class ResNet34SpectrogramClassifier(nn.Module):
         return self.resnet34(x)
 
 
+def train(dataloader, model, loss, optimizer, scheduler, device, cost):
+    model.train()
+    size = len(dataloader.dataset)
+    for batch, (X, Y) in enumerate(dataloader):
+        
+        X, Y = X.to(device), Y.to(device)
+        optimizer.zero_grad()
+        pred = model(X)
+        loss = cost(pred, Y)
+        loss.backward()
+        optimizer.step()
+        
+        if batch % 200 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f'loss: {loss:>7f}  [{current:>5d}/{size:>5d}]')
+            
+    scheduler.step()
+
+def test(dataloader, model, device, cost, log = True):
+    size = len(dataloader.dataset)
+    model.eval()
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for batch, (X, Y) in enumerate(dataloader):
+            X, Y = X.to(device), Y.to(device)
+            pred = model(X)
+            
+            test_loss += cost(pred, Y).item()*batch
+            correct += (pred.argmax(1)==Y.argmax(1)).type(torch.float).sum().item()
+    
+    test_loss /= size
+    correct /= size
+
+    if log:
+        print(f'\nTest Error:\nacc: {(100*correct):>0.1f}%, avg loss: {test_loss:>8f}\n')
+    return correct*100
+
+
+def setup_dataset(train_images = 100, validation_images = 100, test_images = 100, aug = [0,0,0]):
+    #load and one-hot encode labels
+    print('Loading audio data')
+    train_dataset, validation_dataset, test_dataset, labels = load_audio_files('./data/SpeechCommands/speech_commands_v0.02', train_images, validation_images, test_images)
+    train_dataset      = [[pad_waveform(X, 16000), one_hot_encode(label, labels)] for [X, label] in train_dataset]
+    validation_dataset = [[pad_waveform(X, 16000), one_hot_encode(label, labels)] for [X, label] in validation_dataset]
+    test_dataset       = [[pad_waveform(X, 16000), one_hot_encode(label, labels)] for [X, label] in test_dataset]
+    
+    #before converting into spectrograms for training, we do some data augmentation
+    print('Performing waveform augmentations')
+    train_dataset = augment_waveforms(train_dataset, noise = aug[0])
+
+    print("Computing spectrograms")
+    train_dataset = [[waveform_to_spectrogram(waveform), label_vec] for [waveform, label_vec] in train_dataset]
+    validation_dataset = [[waveform_to_spectrogram(waveform), label_vec] for [waveform, label_vec] in validation_dataset]
+    test_dataset =  [[waveform_to_spectrogram(waveform), label_vec] for [waveform, label_vec] in test_dataset]
+    
+    print("Performing spectrogram augmentations")
+    train_dataset = augment_spectrogram(train_dataset, spec_aug = aug[1])
+    
+    return train_dataset, validation_dataset, test_dataset, labels
+
+def full_run(training_images = 100, validation_images = 100, test_images = 100, aug = [0, 0, 0, 0]):
+    train_dataset, validation_dataset, test_dataset, labels = setup_dataset(training_images, validation_images, test_images, aug)
+    output_channels = len(labels)
+        
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=32,
+        num_workers=12,
+        persistent_workers=True,
+        shuffle=True
+    )
+
+    validation_dataloader = DataLoader(
+        validation_dataset,
+        batch_size=32,
+        num_workers=12,
+        persistent_workers=True,
+        shuffle=True
+    )
+    
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    model = ResNet34SpectrogramClassifier(output_channels).to(device)
+    
+    cost = torch.nn.CrossEntropyLoss()
+    learning_rate = 0.0001
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    step_size = 30
+    gamma = 0.1
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+    
+    max_epochs = 100
+    best_epoch = 0
+    best_acc = 0
+    
+    for t in range(max_epochs):
+        print(f'Epoch {t+1}\n-------------------------------')
+        train(train_dataloader, model, cost, optimizer, scheduler, device, cost)
+        acc = test(validation_dataloader, model, device, cost, log = False)
+        
+        if acc > best_acc:
+            best_acc = acc
+            best_epoch = t
+            
+        if (t - best_epoch) > 2:
+            break
+    
+        
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=32,
+        num_workers=12,
+        persistent_workers=True,
+        shuffle=True
+    )
+        
+    test_acc = test(test_dataloader, model, device, cost, log = True)
+            
+    return test_acc
+
+def average_runs(training_images = 100, validation_images = 100, test_images = 100, loops = 1, aug = [0, 0, 0, 0]):
+    accs = np.array([full_run(training_images, validation_images, test_images, aug) for _ in range(loops)])
+    return accs.mean(), accs.std()
+
+if __name__ == '__main__':
+    accs = []
+    
+    train_dataset, validation_dataset, test_dataset, labels = load_audio_files('./data/SpeechCommands/speech_commands_v0.02', 1, 1, 1)
+    wf = train_dataset[0][0]
+    wf = pad_waveform(wf, 16000)
+    spec = waveform_to_spectrogram(wf)
+    
+    plot_spectrogram(spec,  title = 'Non-blurred')
+    
+    spec = add_spec_blur(spec, sigma_time = 0.00001, sigma_freq = 1001)
+    plot_spectrogram(spec, title = 'blurred')
+    
+    
+    aug = [0, 0, 0, 0]
+    #a100, s100 = average_runs(100, 100, 200, 3, aug)
+    #a300, s300 = average_runs(300, 100, 200, 3, aug)
+    #a600, s600 = average_runs(600, 100, 200, 3, aug)
+    #a1000, s1000 = average_runs(1000, 100, 200, 3, aug)
+
+        
+
+
+
+    pass
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 class ConvNeXtTinySpectrogramClassifier(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
@@ -229,199 +430,7 @@ class ConvNeXtSmallSpectrogramClassifier(nn.Module):
 
     def forward(self, x):
         return self.convnext(x)
-
-
-
-# Create the training function
-def train(dataloader, model, loss, optimizer, device, cost):
-    model.train()
-    size = len(dataloader.dataset)
-    for batch, (X, Y) in enumerate(dataloader):
-        
-        X, Y = X.to(device), Y.to(device)
-        optimizer.zero_grad()
-        pred = model(X)
-        loss = cost(pred, Y)
-        loss.backward()
-        optimizer.step()
-        
-        if batch % 200 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f'loss: {loss:>7f}  [{current:>5d}/{size:>5d}]')
-
-
-# Create the validation/test function
-def test(dataloader, model, device, cost):
-    size = len(dataloader.dataset)
-    model.eval()
-    test_loss, correct = 0, 0
-
-    with torch.no_grad():
-        for batch, (X, Y) in enumerate(dataloader):
-            X, Y = X.to(device), Y.to(device)
-            pred = model(X)
-            
-            test_loss += cost(pred, Y).item()*batch
-            correct += (pred.argmax(1)==Y.argmax(1)).type(torch.float).sum().item()
-    
-    test_loss /= size/2
-    correct /= size
-
-    print(f'\nTest Error:\nacc: {(100*correct):>0.1f}%, avg loss: {test_loss:>8f}\n')
-    return correct*100
-
-
-def setup_dataset(aug_noise = True, aug_spec_aug = True, max_images = 100):
-    #load and one-hot encode labels
-    print('Loading audio data')
-    dataset, labels = load_audio_files('./data/SpeechCommands/speech_commands_v0.02', max_images)
-    dataset = [[pad_waveform(X, 16000), fs, one_hot_encode(label, labels)] for [X, fs, label] in dataset]
-
-    
-
-    #split into training and test
-    train_size = int(0.8 * len(dataset))
-    test_size = len(dataset) - train_size
-    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
-
-    #before converting into spectrograms for training, we do some data augmentation
-    print('Performing waveform augmentations')
-    train_dataset = augment_waveforms(train_dataset, noise = aug_noise)
-
-    wf = train_dataset[0][0]
-
-    print("Computing spectrograms")
-    train_dataset = [[waveform_to_spectrogram(waveform, fs), label_vec] for [waveform, fs, label_vec] in train_dataset]
-    test_dataset =  [[waveform_to_spectrogram(waveform, fs), label_vec] for [waveform, fs, label_vec] in test_dataset]
-    
-    print("Performing spectrogram augmentations")
-    train_dataset = augment_spectrogram(train_dataset, spec_aug = aug_spec_aug)
-    
-    return train_dataset, test_dataset, labels
-
-def setup_dataloaders(aug_noise = True, aug_spec_aug = True, max_images = 1500):
-    train_dataset, test_dataset, labels = setup_dataset(aug_noise, aug_spec_aug, max_images)
-
-    train_dataloader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=32,
-        num_workers=12,
-        persistent_workers=True,
-        shuffle=True
-    )
-
-    test_dataloader = torch.utils.data.DataLoader(
-        test_dataset,
-        batch_size=32,
-        num_workers=12,
-        persistent_workers=True,
-        shuffle=True
-    )
-    
-    return train_dataloader, test_dataloader, labels
-
-def run_train_test(train_dataloader, test_dataloader, labels, arch = 'small'):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print('Using {} device'.format(device))
-    
-    model = ConvNeXtSmallSpectrogramClassifier(len(labels)).to(device)
-    if arch == 'tiny':
-        model = ConvNeXtTinySpectrogramClassifier(len(labels)).to(device)
-    elif arch == 'resnet34':
-        model = ResNet34SpectrogramClassifier(len(labels)).to(device)
-    
-    cost = torch.nn.CrossEntropyLoss()
-    learning_rate = 0.0001
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    
-    max_epochs = 100
-    best_epoch = 0
-    best_acc = 0
-    
-    for t in range(max_epochs):
-        print(f'Epoch {t+1}\n-------------------------------')
-        train(train_dataloader, model, cost, optimizer, device, cost)
-        acc = test(test_dataloader, model, device, cost)
-        
-        if acc > best_acc:
-            best_acc = acc
-            best_epoch = t
-            
-        if (t - best_epoch) > 3:
-            break
-        
-    print('BEST:')
-    print(best_acc)
-    print('Done!')
-    return best_acc
-    
-def full_run(max_images, aug_noise, aug_spec_aug, arch, loops = 1):
-    train_dataloader, test_dataloader, labels = setup_dataloaders(max_images = max_images, aug_noise = aug_noise, aug_spec_aug = aug_spec_aug)
-    
-    
-    avg_acc = 0
-    best_acc = 0
-    for _ in range(loops):    
-        acc = run_train_test(train_dataloader, test_dataloader, labels, arch)
-        avg_acc += acc/loops
-        
-        if acc > best_acc:
-            best_acc = acc
-            
-    print('Finished full run with max images {}, noise {}, spec_aug {}, arch{}'.format(max_images, aug_noise, aug_spec_aug, arch))
-    print(avg_acc)
-    print(best_acc)
-            
-    return avg_acc, best_acc
-
-if __name__ == '__main__':
-    accuracies = []
-    
-    #acc_tiny, best_acc_tiny = full_run(1500, True, True, 'tiny', 2)
-    #acc_small, best_acc_small = full_run(1500, True, True, 'small', 2)
-    
-    """
-    limits = np.array(list(range(1, 8)))*200
-    iters = 3
-    
-    for limit in limits:
-        vec = []
-        
-
-        
-        vec.append(acc_no)
-        vec.append(acc_noise)
-        vec.append(acc_spec)
-        accuracies.append(vec)
-    
-    print(accuracies)"""
-    
-    pass
-
-    
-    
 """
-SUMMARY OF RESULTS:
-    
-    Images  Noise  SpecAug  Architecture     Accuracy  
-    1500    Y      Y        ResNet34         92.2%
-    500     Y      Y        ResNet34         88.9%
-    200     Y      Y        ResNet34         84.5%
-    100     Y      Y        ResNet34         79.0%
 
 
-
-
-    1500    Y      Y        ConvNext-tiny    90.5%
-    1500    Y      Y        ConvNext-small   90.5%
-    100     Y      Y        ConvNeXt-tiny    67.6%
-    100     Y      Y        ConvNeXt-small   67.4%    
-    
-        
-
-"""
-    
-
-
-#92.2 % best so far, ResNet34
 
